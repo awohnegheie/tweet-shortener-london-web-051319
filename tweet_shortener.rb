c9213234842fc3b76c799956f7f0e17e19cba876
Awoh Negheie <awohnegheie@gmail.com>
2019-05-10 05:01:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,8 +3,8 @@ def dictionary
     dict = {
     "hello" => 'hi',
     "to" => '2',
-    "two" => '2', 
-    "too" => '2', 
+    "two" => '2',
+    "too" => '2',
     "for" => '4',
     "four" => '4',
     'be' => 'b',
